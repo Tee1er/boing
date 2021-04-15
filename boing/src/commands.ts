9156cb9c3d5732e99c8331510819e59990499f77
Nifley <sailzforjunkmailz@gmail.com>
2021-04-14 19:46:06 -0500

    Restructured, command handler JS not done yet

--- boing/src/commands.ts
+++ boing/src/commands.ts
@@ -1,1 +1,14 @@
 // TODO: Commands refactor
+const fs = require("fs");
+const Discord = require('discord.js');
+const servermgr = require("./server-mgr");
+const client = new Discord.Client();
+const path = require("path");
+const axios = require("axios");
+const chalk = require("chalk");
+
+let commands : {};
+
+function AddCommand(name: string, command: () => void) {
+
+}
