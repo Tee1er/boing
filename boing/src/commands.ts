e10b3df47fa057d9a319592a93db55862ec8a3e3
Nifley <sailzforjunkmailz@gmail.com>
2021-04-15 19:29:41 -0500

    Setup TS config for compiling with JS modules

--- boing/src/commands.ts
+++ boing/src/commands.ts
@@ -1,15 +1,14 @@
 // TODO: Commands refactor
 
-const fs = require("fs");
-const Discord = require('discord.js');
-const servermgr = require("./server-mgr");
+import fs = require("fs");
+import Discord = require('discord.js');
+import servermgr = require("./server-mgr");
 const client = new Discord.Client();
-const path = require("path");
-const axios = require("axios");
-const chalk = require("chalk");
+import path = require("path");
+import axios = require("axios");
+import chalk = require("chalk");
 
 let commands : {};
 
-function AddCommand(name: string, command: () => void) {
-
+function AddCommand(name: string, command: () => void): void {
 }
