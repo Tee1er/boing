54165d7e34c8626d100a5c80a8d0e46236d9270f
Tyler Song <tyler.x.song@gmail.com>
2021-05-25 16:41:30 -0400

    Add unpause command.

---
+++ boing/src/commands/unpause.js
@@ -0,0 +1,15 @@
+const mserver = require("../mserver.js");
+
+let execute = function() {
+    return mserver.write("pause off").then(result => {
+        return `Game unpaused. \`\`\`js\n${result} \`\`\` `
+    })
+}
+
+module.exports = {
+    execute,
+    info: {
+        name: "unpause",
+        descrip: "Unpauses the game."
+    }
+}
