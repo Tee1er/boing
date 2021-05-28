e7e290d0f6ff8ed4f456e1e365110ff4e0851790
Tyler Song <tyler.x.song@gmail.com>
2021-05-28 16:26:45 -0400

    Add ip command.

---
+++ boing/src/commands/ip.js
@@ -0,0 +1,16 @@
+const mserver = require("../mserver.js");
+const { get } = require("axios");
+
+let execute = async function() {
+    response = await get("https://api.ipify.org", {params: {format:"json"}});
+    return `The server's IP is **${response.data.ip}**.`
+}
+
+module.exports = {
+    execute,
+    info: {
+        name: "ip",
+        descrip: "Provides the IP of the server.",
+        longDescrip: "Provides the IP of the server. This does *not* guarantee proper functionality."
+    }
+}
