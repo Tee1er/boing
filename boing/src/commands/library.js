ab71e77cdedecacacf6e307693f26c411c1a8dc8
Tyler Song <tyler.x.song@gmail.com>
2021-06-02 16:43:36 -0400

    Add the library command .

---
+++ boing/src/commands/library.js
@@ -0,0 +1,36 @@
+const mserver = require("../mserver.js");
+const { MessageEmbed } = require("discord.js");
+const { readdirSync, statSync } = require("fs");
+
+let execute = async function() {
+    const paths = readdirSync("../../server/config/saves/boing-library")
+        .filter(element => {
+            return element.endsWith(".msav");
+        })
+    
+    let libraryEmbed = new MessageEmbed()
+        .setColor("#E67B29")
+        .setTitle("Map Library")
+        .setFooter("Boing - github.com/Tee1er/boing");
+    
+    for (path of paths) {
+        let uploadTimestamp = new Date(statSync(`../../server/config/saves/boing-library/${path}`).birthtime)
+
+        let uploadDay = uploadTimestamp.getDate();
+        let uploadMonth = uploadTimestamp.getMonth();
+        let uploadYear = uploadTimestamp.getFullYear();
+        
+        libraryEmbed.addFields({name: path.replace(".msav", ""), value: `Uploaded ${uploadMonth}/${uploadDay}/${uploadYear}.`});
+    }
+
+    return libraryEmbed;
+}
+
+module.exports = {
+    execute,
+    info: {
+        name: "library",
+        descrip: "Lists all the maps in the Map Library.",
+        longDescrip: "Lists maps in the Map Library, in alphabetical order. Play one of them with `<prefix> load`."
+    }
+}
