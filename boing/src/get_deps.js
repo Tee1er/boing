89931709c54df4aebe3b0246cd1f5e53c79457d0
NifleySnifley <sailzforjunkmailz@gmail.com>
2021-09-25 22:20:22 -0500

    Automagically updates server to latest release

--- boing/src/get_deps.js
+++ boing/src/get_deps.js
@@ -1,28 +1,36 @@
 const fs = require('fs');
 const axios = require('axios');
 const http = require('http'); // or 'https' for https:// URLs
-const fetch = (...args) =>
-    import ('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-const SERVER_PATH = "../../server/server.jar";
+const SERVER_PATH = "../../server/";
+const serverinfo_path = SERVER_PATH + "serverinfo.json";
+const server_jar_path = SERVER_PATH + "server.jar";
 
 let get_server = async function(release_url) {
-    if ((!fs.existsSync(SERVER_PATH)) || fs.readFileSync(SERVER_PATH) == "") {
-        console.log(`Mindustry server not found. downloading from ${release_url}...`);
+    const release_metadata = await axios.get(release_url, { responseType: "json" })
+        .then(res => res.data)
+        .catch(err => console.error(err));
 
-        const release_metadata = await axios.get(release_url, { responseType: "json" })
-            .then(res => res.data)
-            .catch(err => console.error(err));
+    const server_info = JSON.parse(fs.existsSync(serverinfo_path) ? fs.readFileSync(serverinfo_path) : "{}");
+
+    if ((!fs.existsSync(server_jar_path)) || fs.readFileSync(server_jar_path) == "" || server_info.release_id != release_metadata.id) {
+        console.log(`Mindustry server not found or different from latest release. Fetching latest release from ${release_url}...`);
 
         // console.log(release_metadata);
-        const server_dl_link = release_metadata["assets"].find(elem => elem["name"].toLowerCase().includes("server"))["browser_download_url"];
+        const server_release = release_metadata["assets"].find(elem => elem["name"].toLowerCase().includes("server"));
 
-        await axios.get(server_dl_link, { responseType: "arraybuffer" })
+        await axios.get(server_release["browser_download_url"], { responseType: "arraybuffer" })
             .then(result => {
                 let data = Buffer.from(result.data);
-                fs.writeFileSync(SERVER_PATH, data, { encoding: null });
+                fs.writeFileSync(server_jar_path, data, { encoding: null });
             });
-        console.log(`\nSuccessfully downloaded latest release of Mindustry server (${release_metadata.name}).`);
+
+        // Write server download metadata for future runs
+        fs.writeFileSync(serverinfo_path,
+            JSON.stringify({ release_id: release_metadata["id"] })
+        );
+
+        console.log(`\nSuccessfully downloaded latest release of Mindustry server (${server_release.name}).`);
     }
 }
 
