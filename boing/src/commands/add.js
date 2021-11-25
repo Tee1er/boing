d3a832663b7c870fc7581e208528059b31f2bfaf
Tyler Song <tyler.x.song@gmail.com>
2021-11-24 22:50:36 -0500

    Fix "add" command.

--- boing/src/commands/add.js
+++ boing/src/commands/add.js
@@ -1,17 +1,23 @@
 const mserver = require("../mserver.js");
 const crypto = require("crypto");
-const { writeFileSync } = require("fs");
+const { writeFileSync, existsSync, mkdirSync } = require("fs");
 const { resolve } = require("path");
 const { get } = require("axios").default;
 const { SERVER_CONFIG_DIR } = require("../globals.js");
 
-let execute = async function(ARGUMENTS, message) {
+let execute = async function (ARGUMENTS, message) {
     if (!ARGUMENTS[1]) {
         return "An error occured. A map name must be provided.";
     }
+    let folderPath = resolve(SERVER_CONFIG_DIR, "./saves/boing-library/");
+    // If backups folder does not exist, create it.
+    if (!existsSync(folderPath)) {
+        mkdirSync(folderPath, { recursive: true });
+    }
     let attachment = message.attachments.array()[0];
     let fileName = ARGUMENTS[1];
-    let filePath = resolve(`${SERVER_CONFIG_DIR}/saves/boing-library/${fileName}.msav`);
+
+    let filePath = folderPath + `/${fileName}.msav`;
     let file = await get(attachment.url, { responseType: "arraybuffer" }).then(result => {
         // eslint-disable-next-line no-undef
         let data = Buffer.from(result.data);
@@ -27,6 +33,6 @@ module.exports = {
     info: {
         name: "add",
         descrip: "Adds a map to the Map Library.",
-        longDescrip: "Adds a map to the Map Library. MSAV format & name for map required. Overrides existing maps with the same name."
-    }
+        longDescrip: "Adds a map to the Map Library. MSAV format & name for map required. Overrides existing maps with the same name.",
+    },
 };
